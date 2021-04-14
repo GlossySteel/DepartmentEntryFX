de0e21c05d5014830e32d5aa729d71d5d43b3cdc
GlossySteel <70051801+GlossySteel@users.noreply.github.com>
2021-04-14 15:01:26 -0700

    Update Assignment6.java

--- Assignment6.java
+++ Assignment6.java
@@ -1,6 +1,5 @@
 // Assignment 6: ASU - CSE 205
 // Name: Joseph Kharzo
-// StudentID: 1218555299
 //Lecture Date and Time: TTH 1:30-2:45
 //  Description: The Assignment6 class creates a Tabbed Pane with
 //               two tabs, one for Department Creation and one for
